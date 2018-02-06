40ba22eddc2523f26fb5b88786ffda1e2975f425
venushub <venu.guntamukkala93@gmail.com>
2018-02-06 13:06:07 +0530

    added variable d

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -7,9 +7,11 @@ public class FirstClass {
 		int a = 10;
 		int b =20 ;
 		int c = 30;
+		int d = 40;
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(c);
+		System.out.println(d);
 		
 	}
 
