909ae7097a69c74d6b2df16b9c5cd573fc6fbb11
venushub <venu.guntamukkala93@gmail.com>
2018-02-06 12:55:37 +0530

    added variable b

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -5,7 +5,9 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 10;
+		int b =20 ;
 		System.out.println(a);
+		System.out.println(b);
 	}
 
 }
