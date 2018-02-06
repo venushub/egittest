3ef7a99e414a5f9f08e2d1d7aaae378817259e74
venushub <venu.guntamukkala93@gmail.com>
2018-02-06 12:54:19 +0530

    first commit

---
+++ src/firstPackage/FirstClass.java
@@ -0,0 +1,11 @@
+package firstPackage;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		System.out.println(a);
+	}
+
+}
